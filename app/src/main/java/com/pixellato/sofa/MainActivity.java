8a621b5534980e2fae5769c98f71a3357a018488
anjandash <anjan8@gmail.com>
2017-11-30 15:10:14 +0100

    first commit

---
+++ app/src/main/java/com/pixellato/sofa/MainActivity.java
@@ -0,0 +1,22 @@
+package com.pixellato.sofa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeText(View view){
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Changed Text!");
+    }
+}
